ffc843ceedd901dccbac51672a9e436a0d75a41a
Nadirameidira <meisaputrinadira@gmail.com>
2026-05-27 22:06:30 +0700

    Menambahkan folder laravel

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use  App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\DB; 
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login'); 
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::resource('posts', PostController::class);
+
+
+Route::get('/vulnerable', function () { 
+$name = request('name'); 
+$user = DB::select("SELECT * FROM users WHERE name = '$name'"); 
+return $user; 
+}); 
+
+Route::get('/', function () {
+    return redirect('/posts');
+});
+
+Route::middleware('auth')->group(function() { 
+Route::resource('posts', PostController::class); 
+}); 
